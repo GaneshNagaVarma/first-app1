ee3d3b3a230e34ab3e442e6bb8f7d91598630e76
Ganesh varma <Ganeshvarmavegiraju@gmail.com>
2025-06-17 16:23:10 +0530

    second

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hello world")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+mysql = MySQL(app)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+    return "Data inserted successfully !"
+
+if __name__ == "__main__":
+    app.run(debug=True)
